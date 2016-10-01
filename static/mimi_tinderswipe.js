0956509c85ef5701132426f9703e4bf53ec89c3d
Mimi Chen <mchen6@nd.edu>
2016-10-01 17:00:27 -0400

    restaurant.json, mimi_tinderswipe.js, index2.html

--- static/mimi_tinderswipe.js
+++ static/mimi_tinderswipe.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-
+    // Function for swiping right
     $(".buddy").on("swiperight",function(){
-      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);      // Rotate image after swiping
       $('.buddy').find('.status').remove();
 
-      $(this).append('<div class="status like">Like!</div>');      
+      $(this).append('<div class="status like">Like!</div>');     // Display the message "Like!"
+
+
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
        } else {
@@ -12,10 +14,11 @@ $(document).ready(function(){
        }
     });  
 
+    // Function for swiping left 
    $(".buddy").on("swipeleft",function(){
-    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);       // Rotate image after swiping
     $('.buddy').find('.status').remove();
-    $(this).append('<div class="status dislike">Dislike!</div>');
+    $(this).append('<div class="status dislike">Dislike</div>');   // Dsiplay the message "Dislike"
 
     if ( $(this).is(':last-child') ) {
      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
@@ -24,4 +27,6 @@ $(document).ready(function(){
     } 
   });
 
+
 });
+
